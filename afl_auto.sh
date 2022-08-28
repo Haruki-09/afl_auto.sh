e0a552f052d513ada47f441d8e6528dd0e8b01a4
kosuge Openbsd <kosuge@tcu.com>
2022-08-28 11:05:50 +0900

    make failure measures. ctrl_c. input commit id.

--- afl_auto.sh
+++ afl_auto.sh
@@ -1,13 +1,24 @@
 #!/usr/local/bin/bash
 
+function ctrl_c {
+
+	echo 'ctrl+C'
+	exit
+}
+
+trap ctrl_c SIGINT
+
 export CC=/usr/local/bin/afl-gcc
 
-commits=/home/kosuge/afl-auto/commit_id/log5.txt
+ls /home/kosuge/afl-auto/commit_id/
+read -p "commit id filename: " inputfile
+commits=/home/kosuge/afl-auto/commit_id/${inputfile}
 in=/home/kosuge/afl-auto/In/
 out=/home/kosuge/afl-auto/Out/
+make_error=/home/kosuge/afl-auto/make_error.log
 seconds=30
 
-echo -e "checkout origin/master now"
+echo -e "\n checkout origin/master now"
 cd /home/kosuge/ctags-link
 git checkout origin/master
 
@@ -21,6 +32,12 @@ do
 	echo -e "\n make now"
 	make clean	
 	make
+
+	if (($? != 0)); then
+		echo -e "\n make failure \n"
+		echo ${commit} >> ${make_error}
+		continue
+	fi
 
 	echo -e "\n make directory now"	
 	mkdir ${out}/${commit}
