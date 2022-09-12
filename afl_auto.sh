709cf6f3e03cab7aac841971913a6e572ad12b34
kosuge Openbsd <kosuge@tcu.com>
2022-09-12 11:54:25 +0900

    commit_id, commit_date, crashes_counts outputgit add .

--- afl_auto.sh
+++ afl_auto.sh
@@ -19,7 +19,8 @@ out=${result_dir}/Out
 in=${afl_auto_dir}/In
 
 echo -e "\n Please select commit id file \n" ; ls ${afl_auto_dir}/commit_id
-read -p "commit id filename: " inputfile && commits=${afl_auto_dir}/commit_id/${inputfile}
+read -p "commit id filename: " input_id && commits=${afl_auto_dir}/commit_id/${input_id}
+#read -p "commit date filename: " input_date && commit_dates=${afl_auto_dir}/commit_id/${input_date}
 
 read -p "Seconds: " sec && seconds=${sec}
 
@@ -28,39 +29,45 @@ touch ${result_dir}/make_error.detail && make_error_detail=${result_dir}/make_er
 touch ${result_dir}/make_success.log && make_success=${result_dir}/make_success.log
 touch ${result_dir}/crashes_count.log && afl_crashes=${result_dir}/crashes_count.log
 
+echo "object_id:	crashes" >> ${afl_crashes}
+
 #echo -e "\n checkout orgin/master"
 cd /home/kosuge/ctags-link
 #git checkout -f origin/master
 
 while read commit
-do	
-	echo -e "\n commit ID: ${commit}"
+do
+	commit_id=`echo ${commit} | cut -d ' ' -f 1`	
+	commit_date=`echo ${commit} | cut -d ' ' -f 2`
+
+	echo -e "\n commit ID: ${commit_id}"
 	
 	echo -e "\n checkout now"
-	git checkout -f ${commit}
+	git checkout -f ${commit_id}
 
 	echo -e "\n make now"
 	make clean
-	echo ${commit} >> ${make_error_detail}
+	echo ${commit_id} >> ${make_error_detail}
 	make 2>> ${make_error_detail}
 
 	if (($? != 0)); then
 		echo -e "\n make failure \n"
-		echo ${commit} >> ${make_error}
+		echo ${commit_id} >> ${make_error}
 		continue
 	fi
 
-	echo ${commit} >> ${make_success}
+	echo ${commit_id} >> ${make_success}
 
 	echo -e "\n make directory now"	
-	mkdir ${out}/${commit}
+	mkdir ${out}/${commit_id}
 	
 	echo -e "\n afl-fuzz run"
-	timeout ${seconds} afl-fuzz -i ${in} -o ${out}/${commit} -f input.c /home/kosuge/ctags-link/ctags input.c
+	timeout ${seconds} afl-fuzz -i ${in} -o ${out}/${commit_id} -f input.c /home/kosuge/ctags-link/ctags input.c
 	
 	echo -e "\n crashes counting"
-	crashes_count=`ls ${out}/${commit} | wc -l`
-	echo "${commit}:${crashes_count}" >> ${afl_crashes}
+	crashes_count=`ls ${out}/${commit_id}/crashes | wc -l`
+	#echo "${commit_id}:${crashes_count},${commit_date}" >> ${afl_crashes}
+	echo "${commit_id}:${commit_date}:${crashes_count}" >> ${afl_crashes}
        	
 	
 done < ${commits}
---
+++ result/2022_09_12_02:25/Out/b8869496424e59fe2266fbcba5d4afd8779c7331/crashes/README.txt
@@ -0,0 +1,17 @@
+Command line used to find this crash:
+
+afl-fuzz -i /home/kosuge/afl-auto/In -o /home/kosuge/afl-auto/result/2022_09_12_02:25/Out/b8869496424e59fe2266fbcba5d4afd8779c7331 -f input.c /home/kosuge/ctags-link/ctags input.c
+
+If you can't reproduce a bug outside of afl-fuzz, be sure to set the same
+memory limit. The limit used for this fuzzing session was 50.0 MB.
+
+Need a tool to minimize test cases before investigating the crashes or sending
+them to a vendor? Check out the afl-tmin that comes with the fuzzer!
+
+Found any cool bugs in open-source tools using afl-fuzz? If yes, please drop
+me a mail at <lcamtuf@coredump.cx> once the issues are fixed - I'd love to
+add your finds to the gallery at:
+
+  http://lcamtuf.coredump.cx/afl/
+
+Thanks :-)
